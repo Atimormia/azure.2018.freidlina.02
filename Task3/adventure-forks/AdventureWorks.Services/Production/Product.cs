6d57c6dbcd2e5e1934eeef85daa00b27c1b44c3c
Palina_Freidlina <palina_freidlina@epam.com>
2018-10-15 10:40:39 +0300

    Task3 API was added

--- Task3/adventure-forks/AdventureWorks.Services/Production/Product.cs
+++ Task3/adventure-forks/AdventureWorks.Services/Production/Product.cs
@@ -15,15 +15,19 @@ namespace AdventureWorks.Services.Production
         public decimal StandardCost { get; set; }
         public decimal ListPrice { get; set; }
         public string Size { get; set; }
-        public UnitMeasure SizeUnitMeasure { get; set; }
-        public UnitMeasure WeightUnitMeasure { get; set; }
+        public string SizeUnitMeasureCode { get; set; }
+        //public UnitMeasure SizeUnitMeasure { get; set; }
+        public string WeightUnitMeasureCode { get; set; }
+        //public UnitMeasure WeightUnitMeasure { get; set; }
         public decimal? Weight { get; set; }
         public int DaysToManufacture { get; set; }
         public string ProductLine { get; set; }
         public string Class { get; set; }
         public string Style { get; set; }
-        public ProductSubcategory ProductSubcategory { get; set; }
-        public ProductModel ProductModel { get; set; }
+        public int? ProductSubcategoryId { get; set; }
+        //public ProductSubcategory ProductSubcategory { get; set; }
+        public int? ProductModelId { get; set; }
+        //public ProductModel ProductModel { get; set; }
         public DateTime SellStartDate { get; set; }
         public DateTime? SellEndDate { get; set; }
         public DateTime? DiscontinuedDate { get; set; }
